04a4bba37a5eda70e4b8985b3c2cef189031389e
Yuri_Antipatrov <89209578005@yandex.ru>
2023-02-10 22:43:31 +0300

    HW for lesson#10

---
+++ data.js
@@ -0,0 +1,19 @@
+const dataProducts = `[{
+    "title": "MANGO  PEOPLE  T-SHIRT",
+    "price": "300",
+    "currency": "USD",
+    "color": "Red",
+    "size": "Xl",
+    "quantuty": 1,
+    "img": "./img/Rectangle 15 copy.jpg"
+},
+{
+    "title": "ELLERY X M'O CAPSULE",
+    "price": "52",
+    "currency": "USD",
+    "color": "Black",
+    "size": "Xl",
+    "quantity": 1,
+    "img": "./img/Rectangle 15 copy (1).jpg"
+}
+]`
